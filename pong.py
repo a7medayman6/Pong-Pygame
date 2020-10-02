58ce6288d4c7b1ec2118787720109128f99352bc
a7medayman6 <a.ayman6000@gmail.com>
2020-10-02 05:26:36 +0200

    added light theme

--- pong.py
+++ pong.py
@@ -12,13 +12,18 @@ black = (0, 0, 0)
 directory = os.getcwd()
 collision_sound = directory + "/sounds/ping_pong_8bit_plop.ogg"
 point_sound = directory + "/sounds/ping_pong_8bit_beeep.ogg"
-
+lightTheme = False
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, key_up, key_down, isAi, ball=None):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([10, 80])
-        self.image.fill(white)
+        #ADD LIGHT THEME
+        
+        if lightTheme:
+            self.image.fill(black)
+        else:
+            self.image.fill(white)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = 260
@@ -41,8 +46,8 @@ class Player(pygame.sprite.Sprite):
                 if self.rect.top > 0:
                     self.rect.y -= 5
         else:
-            self.rect.y = self.ball.rect.y
-
+                self.rect.y = self.ball.rect.y
+        
         game.screen.blit(self.image, (self.rect.x, self.rect.y))
 
 
@@ -50,7 +55,10 @@ class Ball(pygame.sprite.Sprite):
     def __init__(self, direction, speed=2):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([12, 12])
-        self.image.fill(white)
+        if lightTheme:
+            self.image.fill(black)
+        else:
+            self.image.fill(white)
         self.rect = self.image.get_rect()
         self.rect.x = 400
         self.rect.y = choice([10, 590])
@@ -79,14 +87,21 @@ class Pong():
         self.clock = pygame.time.Clock()
 
     def update(self):
-        self.screen.fill(black)
+        #ADD LIGHT THEME
+        if lightTheme:
+            self.screen.fill(white)
+        else:
+            self.screen.fill(black)
         self.ball.update()
         self.player1.update()
         self.player2.update()
         self.show_points()
         self.check_colisions()
         self.check_point()
-        pygame.draw.rect(self.screen, white, (400, 0, 3, 800))
+        if lightTheme:
+            pygame.draw.rect(self.screen, black, (400, 0, 3, 800))
+        else:    
+            pygame.draw.rect(self.screen, white, (400, 0, 3, 800))
         pygame.display.update()
 
     def check_colisions(self):
@@ -123,8 +138,12 @@ class Pong():
         p2_points = str(self.player2.points)
 
         font = pygame.font.Font(directory + '/font/AtariSmall.ttf', 80)
-        text1 = font.render(p1_points, True, white)
-        text2 = font.render(p2_points, True, white)
+        if lightTheme:
+            text1 = font.render(p1_points, True, black)
+            text2 = font.render(p2_points, True, black)
+        else:
+            text1 = font.render(p1_points, True, white)
+            text2 = font.render(p2_points, True, white)
 
         text1_rect = text1.get_rect(center=(200, 50))
         text2_rect = text2.get_rect(center=(600, 50))
@@ -147,25 +166,33 @@ class Pong():
         group = pygame.sprite.Group()
         group.add(Ball(-1))
         group.add(Ball(1))
-
+        global lightTheme
         running = True
         font = pygame.font.Font(directory + '/font/AtariSmall.ttf', 110)
-        title = font.render("Pong", True, white)
+        
+        title = font.render("Pong", True, white)    
         title_rect = title.get_rect(center=(400, 200))
 
         font = pygame.font.Font(directory + '/font/AtariSmall.ttf', 20)
-        subtitle = font.render("Press SPACE to start", True, white)
+        subtitle = font.render("Press SPACE to start in black theme.", True, white)
+        subtitle2 = font.render("Press a to start in white theme", True, white)
         subtitle_rect = subtitle.get_rect(center=(400, 300))
-
+        subtitle2_rect = subtitle2.get_rect(center=(400, 350))    
         while running:
             self.screen.fill(black)
             self.screen.blit(title, title_rect)
             self.screen.blit(subtitle, subtitle_rect)
+            self.screen.blit(subtitle2, subtitle2_rect)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
             if pygame.key.get_pressed()[pygame.K_SPACE]:
+                break
+            elif pygame.key.get_pressed()[pygame.K_a]:
+                
+                lightTheme = True
+                self.__init__()
                 break
 
             for b in group:
@@ -182,6 +209,7 @@ class Pong():
             pygame.display.update()
 
         if running:
+            
             self.main()
         else:
             pygame.quit()
